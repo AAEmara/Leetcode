52050e6cfd6f1566a047d5a3eb00551505a98a70
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2024-11-21 00:11:00 +0200

    [1] [Time Beats: 53.95%] [Memory Beats: 13.56%] - LeetPush

--- Arrays/1-Two-Sum.py
+++ Arrays/1-Two-Sum.py
@@ -1,16 +1,31 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # nums: List[int] = [1, 2, 3, 4]
-        # target: int = 5
-        # 2 + 3 = 5
-        # indices: List[int] = [1, 2]
-        # if target is 6 and the nums are [3, 1, 2], I can't use 3 twice
-        # return the answer in any order
+        # (INPUT) nums: List[int] = [2,3,4,8]
+        # (INPUT) target: int = 6
+        # (OUTPUT) indicies: List[int] = [0, 2]
+        # ASSUMPTIONS: 1. Exactly one solution
+        #                 (e.g., nums = [2,3,2,1]; target = 4, indicies = [0, 2])
+        #              2. You can't use the same element twice 
+        #                 (e.g., nums = [2,3,2,1]; target = 4, indicies = [0, 0]XXX)
+        #              3. You can return the answer in any order
+        #                 (e.g., nums = [2,3,2,1]; target = 4, indicies = [2, 0])
+        #              4. You cannot have only one index in the output
+        #              5. The numbers are not sorted
+        # Suggested Solutions: Two Nested Loops, Time Complexity: O(n^2)
+        # [2, 3, (4), 8], target = 6
+        # [(6), 2, 4, 6], target = 6
+        # [(3), 2, 4], target = 6
+        hash_table = dict() #6: 0, (2): 1, 
+        # 1. For loop on the numbers in the array
         for i in range(len(nums)):
-            # i: 1
-            for j in range(i + 1, len(nums)):
-                # j: [2, 3, 4]
-                sum = nums[i] + nums[j] # 1 + 2, 1 + 3, (1 + 4 = 5)
-                if (sum == target):
-                    # (1 + 4 = 5) == 5
-                    return [i, j] # i: 1, j: 3 [1, 3]
+            # 2. Subtract the number from the target and access the hash table
+            #    to see if there is a value there or not.
+            #    (Key = number, value = index)
+            complement = target - nums[i] # 4, 3, 2
+            if complement in hash_table:
+                # 3. If the complement was found in the hash table, then return
+                #    the value of the key and then current index in an array.
+                return [hash_table[complement], i]
+            # 4. If not, add the current number in the hash table
+            hash_table[nums[i]] = i
+            # 5. repeat from 1 to 4
